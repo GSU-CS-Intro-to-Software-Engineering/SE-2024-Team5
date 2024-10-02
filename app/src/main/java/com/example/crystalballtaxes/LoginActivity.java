88ac8340608a9a13df28fa969801b59aa12129c3
Timi Verissimo <timiv9@gmail.com>
2024-10-02 18:09:44 -0400

    Initial commit

---
+++ app/src/main/java/com/example/crystalballtaxes/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.example.crystalballtaxes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText loginEmailInput, loginPassInput;
+    private TextView forgetPass;
+    private Button loginBtn, signUpBtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginEmailInput = findViewById(R.id.emailEditTxt);
+        loginPassInput = findViewById(R.id.passwordEditTxt);
+
+        forgetPass = findViewById(R.id.forgotPassTxt);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        signUpBtn = findViewById(R.id.signUpBtn);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
