0a8db0173e2aa2fc8646bf993c9fcafc33f52934
Timi Verissimo <timiv9@gmail.com>
2024-10-17 13:46:48 -0400

    Create account UI finished

--- app/src/main/java/com/example/crystalballtaxes/SignUpActivity.java
+++ app/src/main/java/com/example/crystalballtaxes/SignUpActivity.java
@@ -1,19 +1,37 @@
 package com.example.crystalballtaxes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SignUpActivity extends AppCompatActivity {
+    //Components of Sign Up UI
+    private EditText nameTxtF, emailTxtF, passwordTxtF, phoneTxtF;
+    private Button createAccBtn, backBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Assigning id's to Sign Up UI
+        nameTxtF = findViewById(R.id.newAccUserName);
+        emailTxtF = findViewById(R.id.newAccEmail);
+        passwordTxtF = findViewById(R.id.newAccPasswrd);
+        phoneTxtF = findViewById(R.id.newAccPhoneNum);
+
+        createAccBtn =findViewById(R.id.createNewAccBtn);
+    }
+
+    //Takes user back to login page when back arrow button is clicked
+    public void goBack(View v){
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
 
     }
 }
