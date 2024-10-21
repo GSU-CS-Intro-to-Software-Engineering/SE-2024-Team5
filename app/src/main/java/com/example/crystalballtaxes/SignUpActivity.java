312040323853d4a8e213481f53d1cd48c265431c
James White <jamestwhite361@gmail.com>
2024-10-21 01:51:20 -0400

    Signup intial implementation. Progress bar needs styling

--- app/src/main/java/com/example/crystalballtaxes/SignUpActivity.java
+++ app/src/main/java/com/example/crystalballtaxes/SignUpActivity.java
@@ -2,16 +2,28 @@ package com.example.crystalballtaxes;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
     //Components of Sign Up UI
     private EditText nameTxtF, emailTxtF, passwordTxtF, phoneTxtF;
     private Button createAccBtn, backBtn;
+    private ProgressBar signUpProgressBar;
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -24,8 +36,44 @@ public class SignUpActivity extends AppCompatActivity {
         emailTxtF = findViewById(R.id.newAccEmail);
         passwordTxtF = findViewById(R.id.newAccPasswrd);
         phoneTxtF = findViewById(R.id.newAccPhoneNum);
+        signUpProgressBar = findViewById(R.id.signUpProgressBar);
 
         createAccBtn =findViewById(R.id.createNewAccBtn);
+
+        createAccBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signUpProgressBar.setVisibility(View.VISIBLE);
+                String email = emailTxtF.getText().toString();
+                String password = passwordTxtF.getText().toString();
+
+                // Check if email and password are not empty
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(SignUpActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(SignUpActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                signUpProgressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(SignUpActivity.this, "Account created.",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 
     //Takes user back to login page when back arrow button is clicked
@@ -35,41 +83,3 @@ public class SignUpActivity extends AppCompatActivity {
 
     }
 }
-
-/*
-        signUpBtn.setOnClickListener(view -> {
-            progressBar.setVisibility(View.VISIBLE);
-            String email = emailTextEdit.getText().toString();
-            String password = passwordTextEdit.getText().toString();
-
-            // Check if email and password are not empty
-            if(TextUtils.isEmpty(email)){
-                Toast.makeText(SignUpActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            if(TextUtils.isEmpty(password)){
-                Toast.makeText(SignUpActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            // Create user with email and password using firebase template
-            mAuth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                progressBar.setVisibility(View.GONE);
-                                Toast.makeText(SignUpActivity.this, "Account created.",
-                                        Toast.LENGTH_SHORT).show();
-
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Toast.makeText(SignUpActivity.this, "Authentication failed.",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-        });
-        }
-    }
-*/
