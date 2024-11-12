e26f0bc6ef02ead8b0a986bff2546711b5fcd344
Timi Verissimo <timiv9@gmail.com>
2024-11-12 14:05:21 -0500

    Touch up on Dependents UI

--- app/src/main/java/com/example/crystalballtaxes/DependentsActivity.java
+++ app/src/main/java/com/example/crystalballtaxes/DependentsActivity.java
@@ -1,19 +1,32 @@
 package com.example.crystalballtaxes;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class DependentsActivity extends AppCompatActivity {
+
+public class DependentsActivity extends AppCompatActivity  {
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dependents);
 
+        String[] items = new String[] {"Relationship to Taxpayer","Father", "Mother", "Brother", "Sister"};
+        Spinner spinner = (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+
     }
+
 }
